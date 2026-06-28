66c94ce633374eb38d6d15e6f62af22af42d00fc
shuvon miah <shuvonmiah02@gmail.com>
2026-06-29 01:06:04 +0600

    demo commit

--- LanChat/src/main/java/com/chatbox/shuvon/WebSocketConfig.java
+++ LanChat/src/main/java/com/chatbox/shuvon/WebSocketConfig.java
@@ -7,11 +7,6 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
-
-import java.security.Principal;
-import java.util.Map;
-import java.util.UUID;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -32,28 +27,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setUserDestinationPrefix("/user"); // required for private messaging
     }
 
-    /*@Transactional
-    public void save(Entity  a, Entity b) {
-
-        try{
-            Session sessionCleint = hbm.openSession();
-            Session sessionSuper = hbm.openSession();
-
-
-            sessionCleint.persist(a);
-            sessionSuper.persist(b);
-
-
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        finally {
-            if(session != null){
-                close session;
-            }
-        }
-
-    }*/
 
 }
